0eec4ed5489f088374662735064584fcf5875112
soham901 <sagathiyasoham12345@gmail.com>
2023-08-14 05:49:11 +0530

    Updated Styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div>It's Working</div>
+      <div className="bg-zinc-800 flex justify-center items-center h-full w-full">
+        <div className="text-zinc-300 text-6xl main-text">It's Working</div>
+      </div>
     </>
   );
 }
